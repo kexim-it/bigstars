c98b913faa90913af272c3392a00476c33989fc2
kexim-it <t@kingdomeximgroup.com>
2025-08-05 17:14:40 +0000

    updated video heigh and width

--- src/components/BigStarsVideo.jsx
+++ src/components/BigStarsVideo.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const BigStarsVideo = () => {
   const videoId = "1085027360";
   return (
-    <div className="relative w-[40rem] h-[23rem] 3xl:w-[60rem] 3xl:h-[35rem]">
+    <div className="relative lg:w-[40rem] lg:h-[23rem] h-60 md:h-[30rem] sm:h-96 3xl:w-[60rem] 3xl:h-[35rem]">
   <iframe
     className="absolute top-0 left-0 w-full h-full object-cover -z-10"
     src="https://www.youtube-nocookie.com/embed/gaMIljEhGAw?autoplay=1&mute=1&loop=1&playlist=gaMIljEhGAw&modestbranding=1&controls=0&rel=0&showinfo=0"
